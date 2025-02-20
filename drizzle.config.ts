455f5b757163888c13fc0826b80384429c75b52f
Atmajo <atmajoc@gmail.com>
2025-02-21 00:56:50 +0530

    feat: update Drizzle ORM configuration and migration scripts

---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "drizzle-kit"
+
+export default defineConfig({
+  out: "./drizzle/migrations",
+  schema: "./drizzle/db/schema.ts",
+  dialect: "sqlite",
+  driver: "d1-http",
+  dbCredentials: {
+    accountId: "a26a722bd6408842de8098dadebaebe3",
+    databaseId: "1bcd7691-54d4-4fee-b368-4c340a895021",
+    token: "oMmVhkIcI30vCQGRmAc1tMXKeMew8ZBgP6-AghgK"
+  }
+})
